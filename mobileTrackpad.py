1f4ee2f4d62ed4a152bf4c4a53945e841e15d5d6
lukakralj <luka.kralj2@gmail.com>
2020-01-14 02:06:52 +0000

    First demo with moving the mouse

--- mobileTrackpad.py
+++ mobileTrackpad.py
@@ -1,8 +1,12 @@
 import eventlet
 import socketio
+import os
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio)
+
+def moveMouse(dx, dy):
+    os.system("xte \"mousermove " + str(dx) + " " + str(dy) + "\"")
 
 @sio.event
 def connect(sid, environ):
@@ -10,11 +14,13 @@ def connect(sid, environ):
 
 @sio.on("mouse_delta")
 def mouse_delta(sid, data):
-    print('message ', data)
+    moveMouse(data["dx"], data["dy"])
 
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
 
+
 if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(("", 3333)), app)
+
